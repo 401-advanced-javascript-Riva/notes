e7173bfe21dd425872cc773c848b93275f2170c9
RivaD2 <rivkadavidowski@gmail.com>
2020-10-13 19:42:00 -0700

    fixed test for input and cleaned up code

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -3,10 +3,10 @@ const Input = require('../lib/input.js');
 
 
 describe('Testing input', () => {
-test('Given good input, The Class’ valid() method returns true and input module creates a new instance with both action and payload properties', () => {
-const input = new Input({ _: [], ad: 'buy groceries' });
-const input2 = new Input({ _: [], add: 'buy groceries' });
-expect(input2.action).toBeTruthy();
-expect(input.action).toBeFalsy();
-  });
+    test('Given good input, The Class’ valid() method returns true and input module creates a new instance with both action and payload properties', () => {
+        const input = new Input([ '_', [], '-ad', 'buy groceries' ]);
+        const input2 = new Input([ '_', [], '--add', 'buy groceries' ]);
+        expect(input2.action).toBeTruthy();
+        expect(input.action).toBeFalsy();
+    });
 });
--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,10 +1,17 @@
 const Input = require('../lib/input.js');
 
+global.console = {
+    log: jest.fn(),
+    info: jest.fn(),
+    error: jest.fn()
+  }
 
 
 describe('Testing if a command was given', () => {
-test('If the command (add) and data (the note) were both valid, assert that the console shows the output.', () => {
-expect()).toBeTruthy();
-expect().toBeFalsy();
-  });
-});
+test('If the command and data were both valid, the new note should be added to console.', () => {
+    console.log('New Note: Buying groceries')
+    expect(global.console.log).toHaveBeenCalledWith(
+        'My test is working with console.log'
+      )
+    })
+  })
