2b0485aaa1e97cf03353cd642756dce0f19f3801
RivaD2 <rivkadavidowski@gmail.com>
2020-10-13 19:51:51 -0700

    got test to pass for notes.test.js using spy

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,5 +1,6 @@
-const Input = require('../lib/input.js');
+const Notes = require('../lib/notes.js');
 
+//settting up spy
 global.console = {
     log: jest.fn(),
     info: jest.fn(),
@@ -9,9 +10,16 @@ global.console = {
 
 describe('Testing if a command was given', () => {
 test('If the command and data were both valid, the new note should be added to console.', () => {
-    console.log('New Note: Buying groceries')
+    const note = new Notes({action: '--add', payload: 'Feed Ragnar'})
     expect(global.console.log).toHaveBeenCalledWith(
-        'My test is working with console.log'
+        'Added note: Feed Ragnar'
       )
     })
   })
+/* About spy:
+- spy takes a function and replaces it with a magic function that
+allows me to see everything that the function was called with
+- By default when you spy on a function, the actual function will not be
+executed
+- If you want to spy and execute the function you have to specifically say so
+*/
