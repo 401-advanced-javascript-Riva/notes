3419a59f8b5724fb8b1748fe87052916d9aa1555
RivaD2 <rivkadavidowski@gmail.com>
2020-10-16 12:00:16 -0700

    got test for add to pass for collections

--- __tests__/collection.test.js
+++ __tests__/collection.test.js
@@ -7,14 +7,14 @@ const supergoose = require('@code-fellows/supergoose');
 
 describe('testing collections model get method', () => {
   it('Opon add()schema is returned to user with an object with an ID)', () => {
-   return new Notes({ action: 'add', payload:'water succulents'}).then((result) => {
+   return new Notes({ _id:1, text: 'Added note' }).execute({ action: 'add', payload:'water succulents'}).then(result => {
         expect(result.text).toEqual('water succulents');
     });
   })
 })
 
 
-
+//TODO:Implement the test for save
 
 // describe('testing collections model delete method', () => {
 //     it('after doing a delete() user can no longer see the note in a list()  ', () => {
