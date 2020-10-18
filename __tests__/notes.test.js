f7149c0d09cbb4e60d1de7a9f2a8e6d4a864f3c6
RivaD2 <rivkadavidowski@gmail.com>
2020-10-18 11:28:47 -0700

    fixed notes test

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,4 +1,6 @@
 const Notes = require('../lib/notes.js');
+//mocking out the notes collection
+const Collection = jest.createMockFromModule('../model/notes-collection.js').default;
 
 //settting up spy
 global.console = {
@@ -10,12 +12,12 @@ global.console = {
 
 describe('Testing if a command was given', () => {
 test('If the command and data were both valid, the new note should be added to console.', () => {
-    const note = new Notes({action: '--add', payload: 'Feed Ragnar'})
-    expect(global.console.log).toHaveBeenCalledWith(
-        'Added note: Feed Ragnar'
-      )
-    })
-  })
+  const args = {action: '--add', payload: 'Feed Ragnar'}
+  //this test is testing the note calls the collection create method
+    const note = new Notes(args);
+    expect(Collection.create).toHaveBeenCalledWith(args);
+    });
+  });
 /* About spy:
 - spy takes a function and replaces it with a magic function that
 allows me to see everything that the function was called with
